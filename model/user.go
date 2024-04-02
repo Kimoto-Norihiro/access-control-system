1faaece053485ed6fc9e4f7d916d040bfe8e3112
Kimoto-Norihiro <n000r111code@gmail.com>
2024-04-02 17:57:04 +0900

    add json bind

--- model/user.go
+++ model/user.go
@@ -1,6 +1,6 @@
 package model
 
 type User struct {
-	ID      int    `gorm:"column:id;primary_key;auto_increment"`
-	Name    string `gorm:"column:name;not null"`
+	ID   int    `gorm:"column:id;primary_key;auto_increment" json:"id"`
+	Name string `gorm:"column:name;not null" json:"name"`
 }
